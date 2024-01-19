fecf5bcb65fd957ae985c4c6f094ac1a4796a6f9
Richard Garai <garric@okki.hu>
2024-01-19 22:24:14 +0100

    refactor for extension with filters

---
+++ filters/equipment.lua
@@ -0,0 +1,46 @@
+local _, addon = ...
+
+local filter = {}
+
+local sellableItemTypes = {
+    [1] = true,  -- head
+    [2] = true,  -- neck
+    [3] = true,  -- shoulder
+    [4] = true,  -- body
+    [5] = true,  -- chest
+    [6] = true,  -- waist
+    [7] = true,  -- legs
+    [8] = true,  -- feet
+    [9] = true,  -- wrist
+    [10] = true, -- hand
+    [11] = true, -- finger
+    [12] = true, -- trinket
+    [13] = true, -- weapon
+    [14] = true, -- shield
+    [15] = true, -- ranged
+    [16] = true, -- cloak
+    [17] = true, -- 2h-weapon
+    [20] = true, -- robe
+    [21] = true, -- main hand weapon
+    [22] = true, -- off hand weapon
+    [23] = true, -- off hand holdable
+    [26] = true, -- ranged
+    [28] = true, -- relic
+}
+
+function filter:CanSell(item, ilvl_limit)
+    return self:belowThreshold(item, ilvl_limit) and self:isSellable(item)
+end
+
+function filter:belowThreshold(item, ilvl_limit)
+    local ilvl = item:GetCurrentItemLevel()
+    return ilvl > 0 and ilvl <= ilvl_limit
+end
+
+function filter:isSellable(item)
+    local type = item:GetInventoryType()
+    return sellableItemTypes[type]
+end
+
+addon.filters = addon.filters or {}
+addon.filters.equipment = filter
---
+++ seller.lua
@@ -0,0 +1,66 @@
+local _, addon = ...
+
+local seller = {}
+
+addon.Sell = function(ilvl_limit)
+    C_MerchantFrame.SellAllJunkItems()
+    if not ilvl_limit then
+        ilvl_limit = Conf.lowLevelThreshold * GetAverageItemLevel()
+    end
+    seller:ImprovedSellJunkItems(ilvl_limit)
+end
+
+function seller:ImprovedSellJunkItems(ilvl_limit)
+    local itemsInBag = self:bagContents()
+    local itemsToSell = {}
+    for _, item in ipairs(itemsInBag) do
+        item:ContinueOnItemLoad(function()
+            if self:canSell(item, ilvl_limit) then
+                table.insert(itemsToSell, item)
+            end
+        end)
+    end
+    self:throttledSell(itemsToSell)
+end
+
+function seller:bagContents()
+    local res = {}
+    for bag = 0, 4 do
+        for slot = 1, C_Container.GetContainerNumSlots(bag) do
+            local item = Item:CreateFromBagAndSlot(bag, slot)
+            if not item:IsItemEmpty() then
+                table.insert(res, item)
+            end
+        end
+    end
+    return res
+end
+
+function seller:canSell(item, ilvl_limit)
+    local filters = addon.filters or {}
+    for _, filter in pairs(filters) do
+        if filter:CanSell(item, ilvl_limit) then
+            return true
+        end
+    end
+    return false
+end
+
+function seller:throttledSell(items)
+    for i, item in ipairs(items) do
+        if Conf.safeSell and i > Conf.safeSellCount then
+            return
+        end
+        if addon.isMerchantFrameOpen() then
+            C_Timer.After(i * Conf.sellDelay, function()
+                local location = item:GetItemLocation()
+                if location then
+                    C_Container.UseContainerItem(location.bagID, location.slotIndex)    
+                end
+            end)
+        else
+            print(addon.OpenMerchant)
+            return
+        end
+    end
+end
